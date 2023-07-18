124193ed7b9d593d6f5a80ccac45571b8f4bcf1c
Solaris <ovvo.system@gmail.com>
2023-07-18 12:01:29 -0300

    Add variables to keep track of scores

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,6 @@
 const OPTIONS = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const choice = OPTIONS[Math.floor(Math.random() * 3)];
@@ -19,15 +21,49 @@ function playRound(playerSelection, computerSelection) {
         return `Draw! You both picked ${playerSelection}`;
     }
     if (playerSelection === "rock") {
-        if (computerSelection === "paper") return `You lose! ${computerSelection} beats ${playerSelection}.`;
-        if (computerSelection === "scissors") return `You win! ${playerSelection} beats ${computerSelection}.`;
+        if (computerSelection === "paper") {
+            computerScore++;
+            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+        };
+        if (computerSelection === "scissors") {
+            playerScore++;
+            return `You win! ${playerSelection} beats ${computerSelection}.`;
+        }
     }
     if (playerSelection === "paper") {
-        if (computerSelection === "scissors") return `You lose! ${computerSelection} beats ${playerSelection}.`;
-        if (computerSelection === "rock") return `You win! ${playerSelection} beats ${computerSelection}.`;
+        if (computerSelection === "scissors") {
+            computerScore++;
+            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+        }
+        if (computerSelection === "rock") {
+            playerScore++;
+            return `You win! ${playerSelection} beats ${computerSelection}.`;
+        }
     }
     if (playerSelection === "scissors") {
-        if (computerSelection === "rock") return `You lose! ${computerSelection} beats ${playerSelection}.`;
-        if (computerSelection === "paper") return `You win! ${playerSelection} beats ${computerSelection}.`;
+        if (computerSelection === "rock") {
+            computerScore++;
+            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+        }
+        if (computerSelection === "paper") {
+            playerScore++;
+            return `You win! ${playerSelection} beats ${computerSelection}.`;
+        }
+    }
+}
+
+function game() {
+    let playerSelection, computerSelection;
+    let round = 1;
+    while (round <= 5) {
+        computerSelection = getComputerChoice();
+        playerSelection = getPlayerChoice(prompt("Pick rock, paper or scissors"));
+        if (playerSelection) {
+            console.log(playRound(playerSelection, computerSelection));
+        } else {
+            console.log(`That is not a valid choice. Pick one of rock, paper or scissors.`);
+            continue;
+        }
+        round++;
     }
 }
